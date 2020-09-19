527cddff5ab646241b7c208543dfa330acec7320
namtrung <namtrung1901@gmail.com>
2020-09-19 19:16:03 +0700

    post

--- BanDongHo/Controllers/BaivietController.cs
+++ BanDongHo/Controllers/BaivietController.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BanDongHo.Models;
 namespace BanDongHo.Controllers
 {
     public class BaivietController : Controller
     {
+        BanDongHoDBContext db = new BanDongHoDBContext();
         // GET: Baiviet
         public ActionResult Index()
         {
-            return View();
+            var lstTopic = db.Topics.Where(m => m.Status == 1 && m.ParentId == 0).ToList();
+            var Topic = db.Topics.Where(m => m.Status == 1).ToList();
+            ViewBag.ListTopic = Topic;
+            var lstPost = db.Posts.Where(m => m.Status == 1).ToList();
+            ViewBag.ListPost = lstPost;
+            return View(lstTopic);
         }
         public ActionResult PostTopic(String slugtopic)
         {
-            return View();
+            var lstTopic = db.Topics.Where(m => m.Status == 1 && m.Slug == slugtopic).ToList();
+            var lstPost = db.Posts.Where(m => m.Status == 1).ToList();
+            ViewBag.ListPost = lstPost;
+            return View(lstTopic);
         }
         public ActionResult PostDetail(String slugtopic, String slug)
         {
-            return View();
+            var lstTopic = db.Topics.Where(m => m.Status == 1 && m.Slug == slugtopic).ToList();
+            var lstPost = db.Posts.Where(m => m.Status == 1 && m.Slug == slug).ToList();
+            return View(lstPost);
         }
         public ActionResult PostPage(String slug)
         {
